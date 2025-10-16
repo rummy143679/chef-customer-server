f19ddbdf062ea99b9bbd27fd652072e3bc0ea691
matteda ramesh <mattedaramesh27@gmail.com>
2025-10-17 02:47:08 +0530

    deployment error to all apth access

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(frontendPath));
 
     app.get("/*", (req, res) => {
-        res.sendFile(path.resolve(frontendPath, "index.html"));
+        res.sendFile(path.join(frontendPath, "index.html"));
     });
 }
 
