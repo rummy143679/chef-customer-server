a662d95871c1913612448df940b06ca06a1ad15f
matteda ramesh <mattedaramesh27@gmail.com>
2025-10-17 02:19:32 +0530

    configured fronend deployed url in the index.js

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ const dbConnection = require('./DbConnevtion/db');
 const app = express();
 
 app.use(express.json());
+const allowedOrigins = [
+    "http://localhost:5173",             // for local dev
+    "https://enchanting-paprenjak-aa52b4.netlify.app"    // Netlify frontend URL
+];
 
 app.use(
     cors({
-        origin: "http://localhost:5173", // your frontend URL
+        origin: allowedOrigins, // deployed frontend URL
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true
@@ -20,8 +24,17 @@ app.use(
 
 dbConnection();
 
-app.use("/api/v1.0", router); 
+app.use("/api/v1.0", router);
 
+// deployment config for production
+if (process.env.NODE_ENV === "production") {
+    const frontendPath = path.join(__dirname, "../client/dist");
+    app.use(express.static(frontendPath));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(frontendPath, "index.html"));
+    });
+}
 
 
 const port = process.env.PORT || 5000
